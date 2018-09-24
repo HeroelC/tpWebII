3d64069fbbdeba3dc4259cb5b43ae76d07cce112
Leandro Liggerini <liggerinileo@gmail.com>
2018-09-24 17:19:36 -0300

    Funcionalidad Model insertar traer y borrar

--- model/TareasModel.php
+++ model/TareasModel.php
@@ -20,6 +20,24 @@ class TareasModel
     , 'root', '');
   }
 
+  function GetTareas(){
+
+        $sentencia = $this->db->prepare( "select * from tarea");
+        $sentencia->execute();
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function InsertarTarea($titulo,$descripcion,$completada){
+
+      $sentencia = $this->db->prepare("INSERT INTO tarea(titulo, descripcion, completada) VALUES(?,?,?)");
+      $sentencia->execute(array($titulo,$descripcion,$completada));
+    }
+
+    function BorrarTarea($idTarea){
+
+      $sentencia = $this->db->prepare( "delete from tarea where id=?");
+      $sentencia->execute(array($idTarea));
+    }
 
 }
 
